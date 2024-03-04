ff7fc04130bba00475c21f56a6622214fd977392
Nitish Kumar Jha <nitish.jha@Nitishs-MacBook-Pro.local>
2024-03-04 21:50:51 +0530

    Created the selenium grid automation project

---
+++ src/test/java/com/nitish/Testing.java
@@ -0,0 +1,12 @@
+package com.nitish;
+
+import com.nitish.util.ConfigUtil;
+
+public class Testing {
+
+    public static void main(String[] args){
+        System.setProperty("browser", "edge");
+        ConfigUtil.intialize();
+    }
+
+}
---
+++ src/test/java/com/nitish/tests/vendorportal/VendorPortalTest.java
@@ -0,0 +1,55 @@
+package com.nitish.tests.vendorportal;
+
+import com.nitish.pages.vendorportal.DashboardPage;
+import com.nitish.pages.vendorportal.LoginPage;
+import com.nitish.tests.AbstractTest;
+import com.nitish.tests.vendorportal.model.VendorPortalTestData;
+import com.nitish.util.JsonUtil;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class VendorPortalTest extends AbstractTest {
+
+    private LoginPage loginPage;
+    private DashboardPage dashboardPage;
+    private VendorPortalTestData testData;
+
+    @BeforeTest
+    @Parameters("testDataPath")
+    public void setPageObjects(String testDataPath){
+        this.loginPage = new LoginPage(driver);
+        this.dashboardPage = new DashboardPage(driver);
+        this.testData = JsonUtil.getTestData(testDataPath, VendorPortalTestData.class);
+    }
+
+    @Test
+    public void loginTest(){
+        loginPage.goTo("https://d1uh9e7cu07ukd.cloudfront.net/selenium-docker/vendor-app/index.html");
+        Assert.assertTrue(loginPage.isAt());
+        loginPage.login(testData.username(), testData.password());
+    }
+
+    @Test(dependsOnMethods = "loginTest")
+    public void dashboardTest(){
+        Assert.assertTrue(dashboardPage.isAt());
+
+        // finance metrics
+        Assert.assertEquals(dashboardPage.getMonthlyEarning(), testData.monthlyEarning());
+        Assert.assertEquals(dashboardPage.getAnnualEarning(), testData.annualEarning());
+        Assert.assertEquals(dashboardPage.getProfitMargin(), testData.profitMargin());
+        Assert.assertEquals(dashboardPage.getAvailableInventory(), testData.availableInventory());
+
+        // order history search
+        dashboardPage.searchOrderHistoryBy(testData.searchKeyword());
+        Assert.assertEquals(dashboardPage.getSearchResultsCount(), testData.searchResultsCount());
+    }
+
+    @Test(dependsOnMethods = "dashboardTest")
+    public void logoutTest(){
+        dashboardPage.logout();
+        Assert.assertTrue(loginPage.isAt());
+    }
+
+}
